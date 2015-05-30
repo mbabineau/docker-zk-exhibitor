947c4e968de3fc4bba732d61f68b95b1f9aa60f0
Johnny Sheeley <sheeley@aigee.org>
2015-05-30 11:16:19 -0700

    Add proxy support

--- include/wrapper.sh
+++ include/wrapper.sh
@@ -6,12 +6,18 @@ MISSING_VAR_MESSAGE="must be set"
 DEFAULT_AWS_REGION="us-west-2"
 DEFAULT_DATA_DIR="/opt/zookeeper/snapshots"
 DEFAULT_LOG_DIR="/opt/zookeeper/transactions"
+S3_SECURITY=""
+HTTP_PROXY=""
 : ${S3_BUCKET:?$MISSING_VAR_MESSAGE}
 : ${S3_PREFIX:?$MISSING_VAR_MESSAGE}
 : ${HOSTNAME:?$MISSING_VAR_MESSAGE}
 : ${AWS_REGION:=$DEFAULT_AWS_REGION}
 : ${ZK_DATA_DIR:=$DEFAULT_DATA_DIR}
 : ${ZK_LOG_DIR:=$DEFAULT_LOG_DIR}
+: ${HTTP_PROXY_HOST:=""}
+: ${HTTP_PROXY_PORT:=""}
+: ${HTTP_PROXY_USERNAME:=""}
+: ${HTTP_PROXY_PASSWORD:=""}
 
 cat <<- EOF > /opt/exhibitor/defaults.conf
 	zookeeper-data-directory=$ZK_DATA_DIR
@@ -33,17 +39,33 @@ cat <<- EOF > /opt/exhibitor/defaults.conf
 	auto-manage-instances=1
 EOF
 
+
 if [[ -n ${AWS_ACCESS_KEY_ID} ]]; then
   cat <<- EOF > /opt/exhibitor/credentials.properties
     com.netflix.exhibitor.s3.access-key-id=${AWS_ACCESS_KEY_ID}
     com.netflix.exhibitor.s3.access-secret-key=${AWS_SECRET_ACCESS_KEY}
 EOF
+
+  S3_SECURITY="--s3credentials /opt/exhibitor/credentials.properties"
 fi
 
 if [[ -n ${ZK_PASSWORD} ]]; then
 	SECURITY="--security web.xml --realm Zookeeper:realm --remoteauth basic:zk"
 	echo "zk: ${ZK_PASSWORD},zk" > realm
 fi
+
+
+if [[ -n $HTTP_PROXY_HOST ]]; then
+    cat <<- EOF > /opt/exhibitor/proxy.properties
+      com.netflix.exhibitor.s3.proxy-host=${HTTP_PROXY_HOST}
+      com.netflix.exhibitor.s3.proxy-port=${HTTP_PROXY_PORT}
+      com.netflix.exhibitor.s3.proxy-username=${HTTP_PROXY_USERNAME}
+      com.netflix.exhibitor.s3.proxy-password=${HTTP_PROXY_PASSWORD}
+EOF
+
+    HTTP_PROXY="--s3proxy=/opt/exhibitor/proxy.properties"
+fi
+
 
 exec 2>&1
 
@@ -58,17 +80,10 @@ exec 2>&1
 # 	--s3credentials /opt/exhibitor/credentials.properties \
 # 	--s3region us-west-2 --s3backup true
 
-if [[ -n ${AWS_ACCESS_KEY_ID} ]]; then
-  java -jar /opt/exhibitor/exhibitor.jar \
-    --port 8181 --defaultconfig /opt/exhibitor/defaults.conf \
-    --configtype s3 --s3config ${S3_BUCKET}:${S3_PREFIX} \
-    --s3credentials /opt/exhibitor/credentials.properties \
-    --s3region ${AWS_REGION} --s3backup true --hostname ${HOSTNAME} \
-    ${SECURITY}
-else
-  java -jar /opt/exhibitor/exhibitor.jar \
-    --port 8181 --defaultconfig /opt/exhibitor/defaults.conf \
-    --configtype s3 --s3config ${S3_BUCKET}:${S3_PREFIX} \
-    --s3region ${AWS_REGION} --s3backup true --hostname ${HOSTNAME} \
-    ${SECURITY}
-fi
+java -jar /opt/exhibitor/exhibitor.jar \
+  --port 8181 --defaultconfig /opt/exhibitor/defaults.conf \
+  --configtype s3 --s3config ${S3_BUCKET}:${S3_PREFIX} \
+  ${S3_SECURITY} \
+  ${HTTP_PROXY} \
+  --s3region ${AWS_REGION} --s3backup true --hostname ${HOSTNAME} \
+  ${SECURITY}
