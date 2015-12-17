e459534fcae32488f8772f05fdd15329ddbbaa6c
Mike Weliczko <mweliczko@stats.com>
2015-12-17 11:27:41 -0600

    Revert "REVERT ME BEFORE PULL REQUEST: changing a property in zoo-cfg-extra that actually is being changed to see if it takes effect because I changed the new two settings, but it was not reflected in zoo.cfg"
    This reverts commit d294ac4f4bfe436a8309a885f55f2d7a22a67b65.

--- include/wrapper.sh
+++ include/wrapper.sh
@@ -37,7 +37,7 @@ cat <<- EOF > /opt/exhibitor/defaults.conf
 	connect-port=2888
 	observer-threshold=0
 	election-port=3888
-	zoo-cfg-extra=tickTime\=2001&initLimit\=10&syncLimit\=5&quorumListenOnAllIPs\=true&autopurge.snapRetainCount\=$AUTOPURGE_SNAPRETAINCOUNT&autopurge.purgeInterval\=$AUTOPURGE_PURGEINTERVAL
+	zoo-cfg-extra=tickTime\=2000&initLimit\=10&syncLimit\=5&quorumListenOnAllIPs\=true&autopurge.snapRetainCount\=$AUTOPURGE_SNAPRETAINCOUNT&autopurge.purgeInterval\=$AUTOPURGE_PURGEINTERVAL
 	auto-manage-instances-settling-period-ms=0
 	auto-manage-instances=1
 	auto-manage-instances-fixed-ensemble-size=$ZK_ENSEMBLE_SIZE
